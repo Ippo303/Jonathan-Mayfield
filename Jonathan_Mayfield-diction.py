8bd704a8709c463244e02e7018e091368e753923
Jonathan Ryan Mayfield <jonathan.mayfield2@stu.jefferson.kyschools.us>
2018-03-19 08:19:38 -0400

    Second Assignment

---
+++ Jonathan_Mayfield-diction.py
@@ -0,0 +1,8 @@
+Anime = {
+    'one piece':"One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's Weekly Shōnen Jump magazine since July 22, 1997, and has been collected in 88 tankōbon volumes.",
+    'naruto':"Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, an adolescent ninja who searches for recognition from his peers and the village and also dreams of becoming the Hokage, the leader of his village.",
+    'seven deadly sins':"The seven deadly sins, also known as the capital vices or cardinal sins, is a grouping and classification of vices within Christian teachings. Behaviours or habits are classified under this category if they directly give birth to other immoralities.",
+    'hajime no ippo':"Hajime no Ippo is a Japanese boxing manga series written and illustrated by George Morikawa. It has been serialized by Kodansha in Weekly Shōnen Magazine since 1989 and collected in over 119 tankōbon to date.",
+    'fullmetal alchemist':"Fullmetal Alchemist is a Japanese shōnen manga series written and illustrated by Hiromu Arakawa. It was serialized in Square Enix's Monthly Shōnen Gangan magazine between August 2001 and June 2010; the publisher later collected the individual chapters into twenty-seven tankōbon volumes.",
+    'beelzebub':"Beelzebub is a Japanese manga series about a first year student at a school for juvenile delinquents, written and illustrated by Ryūhei Tamura."
+}
